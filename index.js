551fbf9647fd9fc57e11cb4b5ef05eea8c316534
Erik Fazakas <the.fazakas.erik@gmail.com>
2022-10-11 23:24:48 +0200

    Added routes to index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var app = express();
 
 app.use(express.static("static"));
 
+/**
+ * Include all routes.
+ */
+require("./routes/onetask")(app);
+
 var server = app.listen(port, function () {
     console.log("Express running on port " + port + "!");
 });
